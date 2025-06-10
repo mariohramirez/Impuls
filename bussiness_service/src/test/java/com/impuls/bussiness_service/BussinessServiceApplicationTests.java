2ac47b42afd4c0b1932cffb0349b0429bf1ff570
Mario Ramírez <marioh.ramirez@udea.edu.co>
2025-06-10 06:23:25 -0500

    Initial project created

---
+++ bussiness_service/src/test/java/com/impuls/bussiness_service/BussinessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impuls.bussiness_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BussinessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
